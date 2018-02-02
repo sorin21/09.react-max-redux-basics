fdc696a3481515af11b660752a40d494dafcdce2
Sorinel Dragomir <sorinel.dragomir@hub.vauban.ro>
2018-02-02 15:28:30 +0200

    Redux quiz

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -9,6 +9,24 @@ const reducer = (state = initialState, action) => {
       counter: state.counter + 1
     }
   } 
+  if (action.type === 'DECREMENT') {
+    return {
+      ...state,
+      counter: state.counter - 1
+    }
+  } 
+  if (action.type === 'ADD') {
+    return {
+      ...state,
+      counter: state.counter + 5
+    }
+  } 
+  if (action.type === 'SUBSTRACT') {
+    return {
+      ...state,
+      counter: state.counter - 5
+    }
+  } 
   return state;
 }
 
