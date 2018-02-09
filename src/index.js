0c6b16db34ab36b438c41d37b0d44dfc16c133bb
Sorinel Dragomir <sorinel.dragomir@hub.vauban.ro>
2018-02-09 17:08:00 +0200

    Little change to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// install --save redux
+// npm install --save redux
 import { createStore, combineReducers } from "redux";
-// install --save react-redux
+// npm install --save react-redux
 import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
